689f09933ff11000ac641c91efd2ff87d1e6d26d
Hug3O <131569471+hugo529100@users.noreply.github.com>
2025-05-11 13:35:52 +0800

    Add files via upload

---
+++ plugin.js
@@ -0,0 +1,4 @@
+exports.version = 1
+exports.apiRequired = 8.85
+exports.description = "Prevents closing dialogs by tapping outside or pressing ESC. This is especially useful for mobile users where accidental gestures may trigger unwanted dialog closure. Only the close button (X) is allowed to dismiss dialogs."
+exports.frontend_js = 'main.js'
